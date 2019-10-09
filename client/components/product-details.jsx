111b2096d4270884500f4bf7b7b87584de703e84
codechristine <christinele@csu.fullerton.edu>
2019-10-08 19:03:23 -0700

    added back shopping cart but still need to update the add to cart function of the button and update cart item number

---
+++ client/components/product-details.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount(id) {
+    fetch(`/api/products.php?id=${this.props.clicked}`)
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ product: result[0] });
+      })
+      .catch(error => console.error('fetch error:', error));
+  }
+  render() {
+
+    if (this.state.product) {
+      let image = this.state.product.img;
+      let name = this.state.product.productName;
+      let price = '$' + this.state.product.productPrice;
+      let shortDescription = this.state.product.productDescription;
+      let longDescription = <div className="text-wrapper"><LoremIpsum p={2} /> </div>;
+
+      return (
+        <div className='container'>
+          <div onClick={() => { this.props.setView('catalog', '{}'); }} className="mt-4 ml-3" >{'< Back To Catalogue'}</div>
+          <div className='media mb-3 mt-1' style={{ 'max-width': '60%' }}>
+
+            <img className='mr-3 mt-4' style={{ 'height': '30rem' }} src={ image } />
+            <div className='col-md-8'>
+              <h2 className='mt-4'>{name}</h2>
+              <h4 className='mt-2'>{ price }</h4>
+              <p className='mt-3'>{ shortDescription }</p>
+            </div>
+          </div>
+          <p className='media-body'>{ longDescription }</p>
+        </div>
+      );
+    } else {
+      return <h3>PAGE NOT FOUND</h3>;
+    }
+  }
+}
+
+export default ProductDetails;
