6bf06976347a67212f42c7f7031e013042e06b12
codechristine <christinele@csu.fullerton.edu>
2019-10-10 10:51:04 -0700

    added status code ouput to functions file, removed commented out code and exit function

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,13 +1,5 @@
 <?php
 
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
-
 require_once('functions.php');
 require_once('db_connection.php');
 
@@ -17,24 +9,22 @@ startup();
 $query = "SELECT * FROM `product` WHERE 1";
 $result = mysqli_query($conn, $query);
 
-if(!$result){
+if (!$result) {
   throw new Exception('error in query' . mysqli_error($conn));
-  exit();
 }
 
 $output = [
-  'data'=> []
+  'data' => []
 ];
 
-while($row = mysqli_fetch_assoc($result)){
+while ($row = mysqli_fetch_assoc($result)) {
   array_push($output['data'], $row);
 }
 
 $json_output = json_encode($output);
 print($json_output);
 
-// $output = file_get_contents('dummy-products-list.json');
 
-// print($output);
+print($output);
 
 ?>
