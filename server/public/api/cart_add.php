590ac740a845b0cd8213a01866a0d590450933a1
codechristine <christinele@csu.fullerton.edu>
2019-10-11 00:50:38 -0700

    added rest of add to cart back end code. Still need to update functionality to front end and update query. Added get cart file as well.

--- server/public/api/cart_add.php
+++ server/public/api/cart_add.php
@@ -1,10 +1,5 @@
 <?php
 
-require_once('functions.php');
-// set_exception_handler('error_handler');
-
-// require_once('db_connection.php');
-
 if(!defined('INTERNAL')){
   exit('error: no direct access');
 }
@@ -32,12 +27,12 @@ $query = " SELECT `price`
 $result = mysqli_query($conn, $query);
 
 
-if (!$result) {
+if(!$result) {
   throw new Exception('error in query' . mysqli_error($conn));
 }
 
-if (mysqli_num_rows($result) === 0 && $id !== false) {
-  throw new Exception("invalid id: $id");
+if(mysqli_num_rows($result) === 0 && $id !== false) {
+  throw new Exception("invalid id:", $id);
 }
 
 $productData = [];
@@ -46,15 +41,46 @@ while ($row = mysqli_fetch_assoc($result)) {
   array_push($productData, $row);
 }
 
-if ($id) {
+if($id) {
   $productData = $productData[0];
 }
 
 $json_output = json_encode($productData);
 print($json_output);
 
-// START TRANSACTION;
+$query = mysqli_query($conn, 'START TRANSACTION');
 
-// " SELECT * FROM `cartItems "
+if (!$query) {
+  throw new Exception('invalid result');
+}
+
+if($cartID === false){
+  $query = " INSERT INTO `cart` (created) VALUES (NOW()) ";
+  $result = mysqli_query($conn, $query);
+}
+
+$cartID = (mysqli_insert_id($conn));
+$_SESSION['cartID'] = (mysqli_insert_id($conn));
+print_r($cartID);
+print_r($_SESSION['cartID']);
+
+if (mysqli_affected_rows($conn) > 0) {
+  print_r(mysqli_affected_rows($conn));
+} else {
+  throw new Exception('no rows were affected');
+}
+
+$insertedQuery = " INSERT INTO `cartItems` (count, productID, price, added, cartID )
+                    VALUES (1, $id, $query, NOW(), $cartID )
+                    ON DUPLICATE KEY UPDATE count = count + 1 ";
+
+$insertResult = mysqli_query($conn, $insertedQuery);
+print_r($insertResult);
+
+if (mysqli_affected_rows($conn) > 0) {
+  mysqli_query($conn, 'COMMIT');
+} else {
+  mysqli_query($conn, 'ROLLBACK');
+}
 
 ?>
