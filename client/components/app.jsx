1776829cd07a5b6fb9cc5a4221c03c1897e2f825
codechristine <christinele@csu.fullerton.edu>
2019-10-12 02:41:47 -0700

    added bootstrap styling after completing checkoutform and placing order components

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -76,30 +76,38 @@ export default class App extends React.Component {
   render() {
     if (this.state.view.name === 'catalog') {
       return (
-        <div className='container'>
-          <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
-          <ProductList itemAddedToCart={this.addToCart} setView={this.setView} view={this.state.view.params} />
+        <div style={{ 'backgroundSize': 'cover', 'backgroundColor': 'black' }}>
+          <div className='container'>
+            <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
+            <ProductList itemAddedToCart={this.addToCart} setView={this.setView} view={this.state.view.params} />
+          </div>
         </div>
       );
     } else if (this.state.view.name === 'details') {
       return (
-        <div className='container'>
-          <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
-          <ProductDetails setView={this.setView} clicked={this.state.view.params.id} itemAddedToCart={ this.addToCart }/>
+        <div style={{ 'backgroundSize': 'cover', 'backgroundColor': 'black' }}>
+          <div className='container'>
+            <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
+            <ProductDetails setView={this.setView} clicked={this.state.view.params.id} itemAddedToCart={ this.addToCart }/>
+          </div>
         </div>
       );
     } else if (this.state.view.name === 'cart') {
       return (
-        <div className='container'>
-          <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
-          <CartSummary itemAddedToCart={this.addToCart} setView={this.setView} cartState={this.state.cart} />
+        <div style={{ 'backgroundSize': 'cover', 'backgroundColor': 'black' }}>
+          <div className='container'>
+            <Header cartItemCount={this.state.cart.length} setView={this.setView} cartView={this.state.view.name.cart} />
+            <CartSummary itemAddedToCart={this.addToCart} setView={this.setView} cartState={this.state.cart} />
+          </div>
         </div>
       );
     } else if (this.state.view.name === 'checkout') {
       return (
-        <div className='container'>
-          <Header cartItemCount={this.state.cart.length} />
-          <CheckoutForm placeOrder={this.placeOrder} setView={this.setView} checkout={this.state.view.name.checkout} />
+        <div style={{ 'backgroundSize': 'cover', 'backgroundColor': 'black' }}>
+          <div className='container'>
+            <Header cartItemCount={this.state.cart.length} />
+            <CheckoutForm placeOrder={this.placeOrder} setView={this.setView} cartState={this.state.cart} checkout={this.state.view.name.checkout} />
+          </div>
         </div>
       );
     }
