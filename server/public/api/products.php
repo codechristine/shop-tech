c19524a4cd53768eab8161699efcc6c9e3dafd4c
codechristine <christinele.dev@gmail.com>
2019-12-03 13:58:28 -0800

    updated db connection file location into server

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,7 +3,7 @@
 require_once('functions.php');
 set_exception_handler('error_handler');
 
-require_once('db_connection.php');
+require_once('../../db_connection.php');
 startup();
 
 $whereClause = "";
